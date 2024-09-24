697f0235491f7fcbdefb11a8fa936f9b47139a32
JViniX <47263194+JViniX@users.noreply.github.com>
2024-09-23 22:12:05 -0400

    Introduce Express

---
+++ routes/products.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+router.get('/apple', function(req, res, next) {
+  res.send('Hello Apple fanboy!');
+});
+
+module.exports = router;
